4f8fa426544f67a5094a0abe3db1289fd931b830
Brandon Ekins <brandon.ekins@gmail.com>
2016-12-07 14:09:27 -0700

    added inputs

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,12 +1,19 @@
 var app = angular.module("authApp", ["firebase", "ngCookies"]);
 app.controller("authController", function($scope, $firebaseAuth, $cookies, $cookieStore) {
     var auth = $firebaseAuth();
-    // login with google
-
-    $scope.user = JSON.parse($cookies.get('user'));
-
+    //pulls cookies
+    if ($cookies.get('user') != undefined)
+        $scope.user = JSON.parse($cookies.get('user'));
+    if ($cookies.get('card') != undefined)
+        $scope.card = JSON.parse($cookies.get('card'));
+    else
+        $scope.card = {
+            greeting: "",
+            text: "",
+            pic: ""
+        }
     $scope.SignIn = function() {
-        auth.$signInWithPopup("google").then(function(user) {
+        auth.$signInWithPopup("google").then(function(user) { // login with google
 
             $scope.user = user;
             console.log("Signed in as:", $scope.user.user.displayName);
@@ -23,5 +30,10 @@ app.controller("authController", function($scope, $firebaseAuth, $cookies, $cook
         }, function(error) {
             console.log(error);
         });
+    }
+
+    $scope.createCard = function() {
+        $cookies.put('card', JSON.stringify($scope.card));
+        $window.location('showCard.html');
     }
 });
